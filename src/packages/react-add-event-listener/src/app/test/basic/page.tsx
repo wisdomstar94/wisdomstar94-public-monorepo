f7d22dc0fbf0db2a52dabb79bb641f8dc3704cfa
wisdomstar94 <wisdomstar94@gmail.com>
2024-06-16 09:00:41 +0900

    종속성에 명시된 버전을 전체 표기

--- src/packages/react-add-event-listener/src/app/test/basic/page.tsx
+++ src/packages/react-add-event-listener/src/app/test/basic/page.tsx
@@ -1,6 +1,16 @@
-"use client" 
+"use client"
+import { useAddEventListener } from "@/hooks/use-add-event-listener/use-add-event-listener.hook";
 
 export default function Page() {
+  useAddEventListener({
+    windowEventRequiredInfo: {
+      eventName: 'click',
+      eventListener(event) {
+        console.log('@click', event);
+      },
+    }
+  });
+
   return (
     <>
       test/basic!  
