a7426c821eff46966aa962110f12eb93eeb6adee
wisdomstar94 <wisdomstar94@gmail.com>
2024-06-22 08:18:56 +0900

    babylon-js-example 프로젝트 생성, yarn 4.3.1 로 업데이트.

---
+++ src/webs/babylon-js-example/src/app/test/basic/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+export default function Page() {
+  return (
+    <>
+      <div className="w-full flex flex-wrap gap-2 relative">
+        <div className="w-full flex flex-wrap gap-2 relative">
+          <h1 className="text-3xl font-extrabold">/test/basic</h1>
+        </div>
+        <div className="w-full flex flex-wrap gap-2 relative">
+          ...
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/webs/babylon-js-example/tailwind.config.ts
@@ -0,0 +1,4 @@
+import { getCommonTailwindConfig } from "../../libs/packages-common-lib/src/configs/tailwind.common.config.mjs";
+
+const config = getCommonTailwindConfig(undefined);
+export default config;
