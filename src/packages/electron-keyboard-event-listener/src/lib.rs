99470f94e61c35a5e444a20a060ae5d7e3884ba8
wisdomstar94 <wisdomstar94@gmail.com>
2024-06-26 23:35:40 +0900

    electron-keyboard-event-listener 개발 도중 중간 커밋.

---
+++ src/packages/electron-keyboard-event-listener/src/lib.rs
@@ -0,0 +1,62 @@
+use gtk4::{self, gdk, gio::ActionEntry, glib, prelude::{ActionMapExt, ActionMapExtManual, ApplicationExt, ApplicationExtManual, GtkWindowExt, WidgetExt}, Application, ApplicationWindow};
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .width_request(360)
+        .build();
+
+    // Add action "close" to `window` taking no parameter
+    let action_close = ActionEntry::builder("close")
+        .activate(|window: &ApplicationWindow, _, _| {
+            window.close();
+        })
+        .build();
+    window.add_action_entries([action_close]);
+
+    // Present window
+    window.present();
+}
+
+pub fn run() -> glib::ExitCode {
+    gtk4::init().unwrap();
+
+    let application = gtk4::Application::new(
+        Some("com.electron_keyboard_event_listener"),
+        Default::default(),
+    );
+    // application.connect_activate();
+    // application.run();
+
+    let window = gtk4::ApplicationWindow::builder()
+        .application(&application)
+        .build();
+
+    let event_controller = gtk4::EventControllerKey::new();
+
+    event_controller.connect_key_pressed(|_, key, _, _| {
+        match key {
+            gdk::Key::Escape => {
+                std::process::exit(0);
+            },
+            gdk::Key::B => {
+                println!("B 누름!");
+            },
+            gdk::Key::Control_L => {
+                println!("Control_L 누름!");
+            },
+            _ => (),
+        }
+        glib::Propagation::Proceed
+    });
+
+    window.add_controller(event_controller);
+    window.present();
+
+
+    // application.add_action(action)
+    application.connect_activate(build_ui);
+    application.run()
+}
---
+++ src/packages/electron-keyboard-event-listener/src/main.rs
@@ -0,0 +1,6 @@
+use electron_keyboard_event_listener::run;
+use gtk4::glib;
+
+fn main() -> glib::ExitCode {
+    run()
+}
