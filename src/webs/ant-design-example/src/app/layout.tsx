2099420ac3f575bd52da1e3939c69f48d883e8fd
wisdomstar94 <wisdomstar94@gmail.com>
2024-07-04 11:26:27 +0900

    /test/grid 구현

--- src/webs/ant-design-example/src/app/layout.tsx
+++ src/webs/ant-design-example/src/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { CommonRootLayout, ICommonRootLayout } from "#packages-common-lib";
 import { Metadata } from "next";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { ConfigProvider } from 'antd';
+import { theme } from "@/theme/theme.config";
 
 export const metadata: Metadata = {
   title: "ant-design-example",
@@ -13,14 +15,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const menus: ICommonRootLayout.MenuItem[] = [
-    { name: "test/grid", href: "/test/grid" },
+    { name: "/test/grid", href: "/test/grid" },
   ];
 
   return (
     <AntdRegistry>
-      <CommonRootLayout menus={menus}>
-        { children }
-      </CommonRootLayout>
+      <ConfigProvider theme={theme}>
+        <CommonRootLayout menus={menus}>
+          { children }
+        </CommonRootLayout>
+      </ConfigProvider>
     </AntdRegistry>
   );
 }
