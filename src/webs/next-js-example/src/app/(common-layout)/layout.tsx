ca916512078f68d91ef565a7990658c26fd9b62a
wisdomstar94 <wisdomstar94@gmail.com>
2024-07-08 10:41:01 +0900

    next-js-example 레포 생성.

---
+++ src/webs/next-js-example/src/app/(common-layout)/layout.tsx
@@ -0,0 +1,18 @@
+import { CommonRootLayout, ICommonRootLayout } from "#packages-common-lib";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const menus: ICommonRootLayout.MenuItem[] = [
+    { name: 'test/use-id', href: '/test/use-id' },
+    { name: 'test/use-id2', href: '/test/use-id2' },
+  ];
+
+  return (
+    <CommonRootLayout menus={menus}>
+      { children }
+    </CommonRootLayout>
+  );
+}
---
+++ src/webs/next-js-example/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'next-js-example'
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>
+        { children }
+      </body>
+    </html>
+  )
+};
