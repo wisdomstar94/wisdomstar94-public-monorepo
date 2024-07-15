416f7fc072f13f3d75744544b159aba4d791f63d
wisdomstar94 <wisdomstar94@gmail.com>
2024-07-15 14:59:08 +0900

    next.js middleware.ts 내용 수정.

--- src/webs/next-js-example/src/middleware.ts
+++ src/webs/next-js-example/src/middleware.ts
@@ -1,19 +1,35 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export default async function middleware(request: NextRequest) {
-  console.log('@middleware 호출됨..!');
-  console.log('@request.method', request.method);
-  console.log('@request.url', request.url);
-  try {
-    console.log('@request.body', await request.json());
-  } catch (e) {
-    console.error('@request.body.error', e);
-  }
-  console.log('@request.ip', request.ip);
-  console.log('@request.headers', request.headers);
-  console.log('@request.cookies', request.cookies);
+  // console.log('@middleware 호출됨..!');
+  // console.log('@request.method', request.method);
+  // console.log('@request.url', request.url);
+  // try {
+  //   console.log('@request.body', await request.json());
+  // } catch (e) {
+  //   console.error('@request.body.error', e);
+  // }
+  // console.log('@request.ip', request.ip);
+  // console.log('@request.headers', request.headers);
+  // console.log('@request.cookies', request.cookies);
 
-  const response = new NextResponse();
-  response.headers.set('x-your-custom-header', '...');
-  return response;
+  // request header control
+  const requestHeaders = new Headers(request.headers);
+
+  // Add new request headers
+  requestHeaders.set('x-hello-from-middleware1', 'hello');
+  requestHeaders.set('x-hello-from-middleware2', 'world!');
+
+  // Update an existing request header
+  requestHeaders.set('user-agent', 'New User Agent overriden by middleware!');
+
+  // Delete an existing request header
+  requestHeaders.delete('x-from-client');
+
+  return NextResponse.next({
+    request: {
+      // New request headers
+      headers: requestHeaders,
+    },
+  });
 }
