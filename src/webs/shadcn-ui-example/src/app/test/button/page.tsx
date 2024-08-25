f63ceb35ca84244e8de1ffb580debeee3bb88e3e
wisdomstar94 <wisdomstar94@gmail.com>
2024-08-25 09:49:31 +0900

    shadcn-ui 셋팅.

--- src/webs/shadcn-ui-example/src/app/test/button/page.tsx
+++ src/webs/shadcn-ui-example/src/app/test/button/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@/components/ui/button';
 import { FC } from 'react';
 
 const Page: FC = () => {
-  return <>button..</>;
+  return (
+    <>
+      <Button>버튼 입니다.</Button>
+      <Button variant="secondary">버튼 입니다.</Button>
+      <Button variant="outline">버튼 입니다.</Button>
+    </>
+  );
 };
 
 export default Page;
