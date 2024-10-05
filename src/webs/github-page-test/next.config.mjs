ee84a8ac83cda6c2909e398e299a5488b097befe
wisdomstar94 <wisdomstar94@gmail.com>
2024-10-05 16:57:12 +0900

    basePath 추가.

--- src/webs/github-page-test/next.config.mjs
+++ src/webs/github-page-test/next.config.mjs
@@ -3,6 +3,12 @@ import createMDX from '@next/mdx';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+
+  /**
+   * 배포된 github pages 가 https://wisdomstar94.github.io/wisdomstar94-public-monorepo 으로 시작하므로
+   * basePath 를 "/wisdomstar94-public-monorepo" 으로 지정.
+   */
+  basePath: '/wisdomstar94-public-monorepo',
   output: 'export',
 };
 
